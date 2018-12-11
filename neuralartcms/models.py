2224609823061767e3e3fa6741dd4f8dd45d4c9e
isboj <c2007th@gmail.com>
2018-12-11 14:27:27 +0900

    add: サムネイルの表示のため、ImageKitで複数画像を生成するようにした

--- neuralartcms/models.py
+++ neuralartcms/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from accounts.models import User
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+from imagekit.processors import ResizeToFit
 
 
 class Material(models.Model):
@@ -8,10 +11,45 @@ class Material(models.Model):
     """
     user = models.ForeignKey(User, related_name='materials', on_delete=models.CASCADE)
     material_name = models.CharField(max_length=100)
+
+    # TODO: 幅は縮小する処理をしているが、高さに関する処理がない
+    # ResizeToFillやSmartResizeを使えばよいかも。調査の必要あり。
     content_image = models.ImageField(upload_to='images/material/content/')
+    content_image_xs = ImageSpecField(source='content_image',
+                                             processors=[ResizeToFit(width='150')],
+                                             format='JPEG',
+                                             options={"quality": 60})
+    content_image_sm = ImageSpecField(source='content_image',
+                                         processors=[ResizeToFit(width='500', upscale=False)],
+                                         format='JPEG',)
+
     content_segmap = models.ImageField(upload_to='images/material/content_segmap')
+    content_segmap_xs = ImageSpecField(source='content_segmap',
+                                              processors=[ResizeToFit(width='150')],
+                                              format='JPEG',
+                                              options={"quality": 60})
+    content_segmap_sm = ImageSpecField(source='content_segmap',
+                                          processors=[ResizeToFit(width='500', upscale=False)],
+                                          format='JPEG',)
+
     style_image = models.ImageField(upload_to='images/material/style/')
+    style_image_xs = ImageSpecField(source='style_image',
+                                           processors=[ResizeToFit(width='150')],
+                                           format='JPEG',
+                                           options={"quality": 60})
+    style_image_sm = ImageSpecField(source='style_image',
+                                       processors=[ResizeToFit(width='500', upscale=False)],
+                                       format='JPEG',)
+
     style_segmap = models.ImageField(upload_to='images/material/style_segmap')
+    style_segmap_xs = ImageSpecField(source='style_segmap',
+                                            processors=[ResizeToFit(width='150')],
+                                            format='JPEG',
+                                            options={"quality": 60})
+    style_segmap_sm = ImageSpecField(source='style_segmap',
+                                        processors=[ResizeToFit(width='500', upscale=False)],
+                                        format='JPEG',)
+
     parameters = models.TextField(blank=True)  # パラメータ調整値、Jsonで格納すること
     start_at = models.DateTimeField(blank=True)
     great_result = models.CharField(max_length=100)
